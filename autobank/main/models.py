bbd0cdbf817e4f7acbe838c84b3230d3fe839758
Michael Hackett <hackett1@sas.upenn.edu>
2021-07-06 16:10:55 -0400

    updates 7.6

--- autobank/main/models.py
+++ autobank/main/models.py
@@ -9,20 +9,26 @@ class PurchaseType(models.Model):
     def __str__(self):
         return str(self.type)
 
+class Shop(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return str(self.name)
+
 
 class Purchase(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    shop = models.CharField(max_length=200)
+    shop = models.ForeignKey(Shop, related_name="purchases", on_delete=models.CASCADE)
     description = models.TextField()
     amount = models.IntegerField(blank=False)
-    bought_for = models.CharField(choices=[('MICHELLE', 'just michelle'), (
-        'MICHAEL', 'just michael'), ('BOTH', 'both of us')], max_length=200)
-    date = models.DateTimeField(auto_created=True, auto_now=True)
+    bought_for = models.CharField(max_length=200)
+    date = models.DateField()
 
     # acts as a foreign key which lets us query all purchases hashed on purchase type
     purchase_type = models.ForeignKey(PurchaseType, related_name="purchases", on_delete=models.CASCADE)
     purchased_by = models.ForeignKey(User, related_name="purchases", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.amount} of ${self.price} at {self.shop} for {self.description} on {self.date}'
+        return f'{self.amount} of ${self.price} at {self.shop.name} for "{self.description}" by {self.purchased_by.username} on {self.date}'
--- autobank/main/views.py
+++ autobank/main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from main.models import PurchaseType, Purchase
+from main.models import PurchaseType, Purchase, Shop
 from django.contrib.auth.decorators import login_required
 
 import csv
 import time
 from django.http import HttpResponse
+from datetime import date
 
 # Create your views here.
 
@@ -14,7 +15,8 @@ from django.http import HttpResponse
 def splash(request):
     if request.user.is_authenticated:
         types = PurchaseType.objects.all()
-        return render(request, 'home.html', {'user': request.user, 'purchase_types': types})
+        shops = Shop.objects.all()
+        return render(request, 'home.html', {'user': request.user, 'purchase_types': types, 'shops': shops})
     return render(request, "splash.html", {})
 
 
@@ -39,22 +41,25 @@ def logout_(request):
 @login_required
 def add_purchase(request):
     if request.method == 'POST':
-        bought_for = request.POST.getlist("for_choices")[0]
         purchase_type = request.POST.getlist("type_choices")[0]
 
         print(f'purchase_type received is {purchase_type}')
         purchase_type_obj = PurchaseType.objects.get(type=purchase_type)
         print(f'object found is {purchase_type_obj}')
         print(f'Creating purchase with fields {request.POST["price"]}', 
-            f'{request.POST["shop"]}, {request.POST["description"]}, {request.POST["amount"]}', bought_for, purchase_type_obj, User.objects.get(username=request.user))
+            f'{request.POST["shop"]}, {request.POST["description"]}, {request.POST["amount"]}', purchase_type_obj, User.objects.get(username=request.user))
 
+        # if amount is null default to 1
+        amount = request.POST['amount'] if request.POST['amount'] else 1
+        dt = request.POST['date'] if request.POST['date'] else date.today()
 
         purchase = Purchase.objects.create(
             price=request.POST['price'],
-            shop=request.POST['shop'],
+            shop=Shop.objects.get(name=request.POST['shop']),
+            date=dt,
             description=request.POST['description'],
-            amount=request.POST['amount'],
-            bought_for=bought_for,
+            amount=amount,
+            bought_for=request.POST['for_choices'],
             purchase_type=purchase_type_obj,
             purchased_by=User.objects.get(username=request.user)
         )
@@ -62,3 +67,19 @@ def add_purchase(request):
         purchase.save()
         return redirect('/')
     else: return redirect('/')
+
+@login_required
+def new_purchase_type(request):
+    if request.method == 'POST':
+        if request.POST['type']:
+            new_type = PurchaseType.objects.create(type=request.POST['type'])
+            new_type.save()
+    return redirect('/')
+
+@login_required
+def new_shop(request):
+    if request.method == 'POST':
+        if request.POST['shop']:
+            new_shop = Shop.objects.create(name=request.POST['shop'])
+            new_shop.save()
+    return redirect('/')

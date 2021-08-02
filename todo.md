9abe5f92b83506cd13a05251575a1cf1f8d39389
Michael Hackett <hackett1@sas.upenn.edu>
2021-08-02 18:33:08 -0400

    whatever

---
+++ todo.md
@@ -0,0 +1,17 @@
+# Abababa Autobank
+
+## Done already
+
+- Build localhost Django webapp to add purchases made by michelle or myself
+- Add endpoint to see stats on spending for individual and joint purchases
+- Add graphing to stats page
+- Deploy site to ec2
+- Add models for Interpayments, Subscription
+- Write backup script (make json files in s3) and deploy to Lambda
+
+## To do
+
+- Put together code deploy rather than manual deployments through ssh/tmux
+- Add Paycheck model to Django
+- Add interpayments, subscription, paycheck to stats page
+- Send weekly and monthly spending/budget reports
---
+++ weekly_spending_emails_lambda/spending_emailer.py
@@ -0,0 +1,64 @@
+"""
+Script processes all purchases from the last week and aggregates spending, sending an email report to michael.
+The email body contains the aggregate values and contains a csv attachment to an itemized spending list.
+
+Prerequirements: OS variables must be set
+
+Assumptions: We assume and hardcode the name of the mysql db and table names. We assume RDS variables will remain constant.
+"""
+
+import json
+import os
+import sys
+import pymysql
+import datetime
+
+def get_yesterday_dt():
+    today = datetime.datetime.today()
+    yesterday = today - datetime.timedelta(days=1)
+    return yesterday
+
+
+def get_rows_to_str(table_name):
+    yesterday = get_yesterday_dt().date()
+    day = yesterday.day
+    month = yesterday.month
+    year = yesterday.year
+
+    # cursor object executes a query and stores the result in itself, which is an iterator.
+    connection = pymysql.connect(host=os.environ['RDS_HOSTNAME'], user=os.environ['RDS_USERNAME'], password=os.environ['RDS_PASSWORD'],
+                             db='', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    cur = connection.cursor()
+    cur.execute(f'use {os.environ["RDS_DB_NAME"]}')
+    cur.execute(f'SELECT * FROM {table_name} WHERE DAY(date)="{day}" AND MONTH(date)="{month}" AND YEAR(date)="{year}"')
+
+    items = [row for row in cur]
+
+    # need to convert objs to a string so that it's json serializable
+    for item in items:
+        item['date'] = str(item['date'])
+        item['price'] = str(item['price'])
+
+    items_json = json.dumps(items, indent=4)
+    return items_json
+
+
+def spending_emailer():
+    # get last week's purchases
+
+    daily_purchases = get_rows_to_str('main_purchase')
+    daily_interpayments = get_rows_to_str('main_interpayment')
+    
+    encoded_purchases = daily_purchases.encode("utf-8")
+    encoded_interpayments = daily_purchases.encode("utf-8")
+
+    if len(encoded_purchases):
+        s3.put_object(Bucket=BUCKET_NAME, Key=file_path + '/purchases.json', Body=encoded_purchases)
+    if len(encoded_interpayments):
+        s3.put_object(Bucket=BUCKET_NAME, Key=file_path + '/interpayments.json', Body=encoded_interpayments)
+    
+
+# for local testing
+if __name__ == '__main__':
+    spending_emailer(sys.argv[1])
+
